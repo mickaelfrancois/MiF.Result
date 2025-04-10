1d0ab45b92f5369a26bc7f9bddcec8fd71754a3b
Mickaël FRANCOIS <mickael.francois@fpc-france.com>
2025-04-10 21:33:36 +0200

    Add base class

--- MiF.Result/Result.cs
+++ MiF.Result/Result.cs
@@ -4,32 +4,12 @@ using MiF.Result.Models;
 namespace MiF.Result;
 
 
-public class Result : IResult
+public class Result : BaseResult, IResult
 {
-    public bool IsSuccess { get; }
-
-    public bool IsError => !IsSuccess;
-
-    public IError? Error { get; }
-
-
     private Result(bool success, IError? error)
     {
         IsSuccess = success;
         Error = error;
-    }
-
-    public bool IsErrorType<TError>() where TError : IError
-    {
-        return Error is TError;
-    }
-
-    public TError GetError<TError>() where TError : IError
-    {
-        if (Error is TError error)
-            return error;
-
-        throw new InvalidOperationException($"The error is not of type {typeof(TError).Name}");
     }
 
     public static Result Success() => new(true, null);
--- MiF.Result/ResultT.cs
+++ MiF.Result/ResultT.cs
@@ -4,15 +4,9 @@ using MiF.Result.Models;
 namespace MiF.Result;
 
 
-public class Result<TValue> : IResultT<TValue>
+public class Result<TValue> : BaseResult, IResultT<TValue>
 {
-    public bool IsSuccess { get; }
-
-    public bool IsError => !IsSuccess;
-
     public TValue? Value { get; }
-
-    public IError? Error { get; }
 
 
     private Result(bool success, TValue? value, IError? error)
@@ -20,19 +14,6 @@ public class Result<TValue> : IResultT<TValue>
         IsSuccess = success;
         Value = value;
         Error = error;
-    }
-
-    public bool IsErrorType<TError>() where TError : IError
-    {
-        return Error is TError;
-    }
-
-    public TError GetError<TError>() where TError : IError
-    {
-        if (Error is TError error)
-            return error;
-
-        throw new InvalidOperationException($"The error is not of type {typeof(TError).Name}");
     }
 
     public static Result<TValue> Success(TValue value) => new(true, value, null);
